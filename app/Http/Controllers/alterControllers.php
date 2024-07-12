a9b52e436a6659f621097426a705bc436c533b02
Taufik <muidmujadi@gmail.com>
2024-07-12 22:32:07 +0700

    initial commit

---
+++ app/Http/Controllers/alterControllers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Alternatif;
+
+class alterControllers extends Controller
+{
+  
+}
